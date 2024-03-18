1d78d0faec6d5530addc6e30e167bd2d8982c137
Filipp Goriainov <barrios1991phil@gmail.com>
2024-03-18 23:06:05 +0400

    add space and arrow

--- src/features/PlayerControls/PlayPauseButton/PlayPauseButton.tsx
+++ src/features/PlayerControls/PlayPauseButton/PlayPauseButton.tsx
@@ -2,6 +2,7 @@ import { ReactComponent as Pause } from '@/shared/icons/pause.svg';
 import { ReactComponent as Play } from '@/shared/icons/play.svg';
 import { ControlButtonWrapper } from '@/shared/ControlButtonWrapper/ControlButtonWrapper';
 import { usePlayerStore } from '@/store/PlayerStore';
+import { useCallback, useEffect } from 'react';
 import cls from './PlayPauseButton.module.scss';
 
 export function PlayPauseButton() {
@@ -14,6 +15,21 @@ export function PlayPauseButton() {
             setIsPlaying(true);
         }
     };
+    const spaceHandler = useCallback((e:KeyboardEvent) => {
+        if (e.key === ' ') {
+            e.preventDefault();
+            if (isPlaying) {
+                setIsPlaying(false);
+            } else {
+                setIsPlaying(true);
+            }
+        }
+    }, [isPlaying, setIsPlaying]);
+
+    useEffect(() => {
+        window.addEventListener('keydown', spaceHandler);
+        return () => { window.removeEventListener('keydown', spaceHandler); };
+    }, [spaceHandler]);
     return (
         <ControlButtonWrapper onClick={play} className={cls.button}>
             {isPlaying ? <Pause /> : <Play />}
--- src/features/TimeLine/ui/TimeLine.tsx
+++ src/features/TimeLine/ui/TimeLine.tsx
@@ -3,7 +3,7 @@
 import { Timer } from '@/shared/Timer';
 import { StyledRange } from '@/shared/StyledRange';
 import {
-    ChangeEvent, useEffect, useMemo, useState,
+    ChangeEvent, useCallback, useEffect, useMemo, useState,
 } from 'react';
 import { timeFormat } from '@/shared/utils/timeFormat/timeFormat';
 import { usePlayerStore } from '@/store/PlayerStore';
@@ -12,7 +12,7 @@ import clsx from 'clsx';
 import cls from './TimeLine.module.scss';
 
 interface TimeLineProps {
-    className?:string
+    className?: string
 }
 
 export function TimeLine({ className }: TimeLineProps) {
@@ -31,12 +31,30 @@ export function TimeLine({ className }: TimeLineProps) {
 
     const totalTime = useMemo(() => (timeFormat(player?.duration || 0)), [player?.duration]);
 
-    const handleChange = (e:ChangeEvent<HTMLInputElement>):void => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
         if (player?.currentTime) {
             player.currentTime = +e.target.value;
         }
     };
 
+    const arrowHandler = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'ArrowLeft') {
+            e.preventDefault();
+            if (player?.currentTime) {
+                player.currentTime -= 5;
+            }
+        } else if (e.key === 'ArrowRight') {
+            e.preventDefault();
+            if (player?.currentTime) {
+                player.currentTime += 5;
+            }
+        }
+    }, [player]);
+
+    useEffect(() => {
+        window.addEventListener('keydown', arrowHandler);
+        return () => { window.removeEventListener('keydown', arrowHandler); };
+    }, [arrowHandler]);
     return (
         <div className={clsx(className, cls.wrapper)}>
             <Timer time={currentTime} />
