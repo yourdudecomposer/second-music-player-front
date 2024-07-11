23eed5004842817cc5f88d276a3fb3c785c84268
Filipp Goriainov <barrios1991phil@gmail.com>
2024-07-11 19:58:42 +0400

    add mail and social

--- src/widgets/Contacts/ui/Contacts.tsx
+++ src/widgets/Contacts/ui/Contacts.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import { ReactComponent as Vk } from '@/shared/icons/vk.svg';
 import { ReactComponent as Youtube } from '@/shared/icons/youtube.svg';
 import { ReactComponent as Telegram } from '@/shared/icons/telegram.svg';
+import { ReactComponent as Spotify } from '@/shared/icons/spotify.svg';
 import { SocialSvgWrapper } from '@/shared/SocialSvgWrapper/SocialSvgWrapper';
 import cls from './Contacts.module.scss';
 
@@ -12,19 +13,19 @@ interface ContactsProps {
 export function Contacts({ className }: ContactsProps) {
     return (
         <div className={clsx(className, cls.div, 'plain_text_size')}>
-            <a className={cls.mail_link} href="mailto:mail@mail.ru&body=привет?subject=вопрос">mailtomail@mail.ru</a>
+            <a className={cls.mail_link} href="mailto:goriainovfilipp@yandex.ru&body=привет?subject=вопрос">mailtomail@mail.ru</a>
             <div className={cls.social_wrapper}>
-                <SocialSvgWrapper href="https://www.youtube.com/" color="red">
+                <SocialSvgWrapper href="https://www.youtube.com/@filippgoriainov6371" color="red">
                     <Youtube />
                 </SocialSvgWrapper>
-                <SocialSvgWrapper href="https://www.youtube.com/" color="#0077FF">
+                <SocialSvgWrapper href="https://vk.com/artist/filippgoriainov" color="#0077FF">
                     <Vk />
                 </SocialSvgWrapper>
-                <SocialSvgWrapper className={cls.tg_moving} href="https://www.youtube.com/" color="#2aabee">
+                <SocialSvgWrapper className={cls.tg_moving} href="https://t.me/guitarandorchestra" color="#2aabee">
                     <Telegram />
                 </SocialSvgWrapper>
-                <SocialSvgWrapper href="https://www.youtube.com/" color="red">
-                    <Vk />
+                <SocialSvgWrapper color="transparent" className={cls.spotify_icon_wrapper} href="https://open.spotify.com/artist/676YDxdfj0g6e5deXEuXi0">
+                    <Spotify />
                 </SocialSvgWrapper>
             </div>
         </div>
