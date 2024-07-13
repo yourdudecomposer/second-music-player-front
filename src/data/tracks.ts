bf71772f61ed24e0ab6b08ed4114700ef53277ba
Filipp Goriainov <barrios1991phil@gmail.com>
2024-07-13 13:08:40 +0400

    fix layout

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -4,8 +4,8 @@ const tracks:ITrack[] = [
     {
         id: 1,
         title: {
-            en: 'Guitar and Orchestra, No. 1',
-            ru: 'Гитара и Оркестр, No 1',
+            en: 'Guitar and Orchestra,\nNo. 1',
+            ru: 'Гитара и Оркестр,\nNo 1',
         },
         description: {
             en: 'A short piece with a climax towards the end of it. written in 2020',
@@ -18,8 +18,8 @@ const tracks:ITrack[] = [
     {
         id: 2,
         title: {
-            en: 'Guitar and Orchestra, No. 2',
-            ru: 'Гитара и Оркестр, No 2',
+            en: 'Guitar and Orchestra,\nNo. 2',
+            ru: 'Гитара и Оркестр,\nNo 2',
         },
         description: {
             en: 'Guitar tuned to Drop C, lots of emotion and bells',
@@ -33,11 +33,11 @@ const tracks:ITrack[] = [
         id: 3,
         title: {
             en: 'Guitar, Duduk and Orchestra',
-            ru: 'Гитар, Дудук  и Оркестр',
+            ru: 'Гитар, Дудук\nи Оркестр',
         },
         description: {
             en: 'I really like the sound of the duduk. Made it a solo.',
-            ru: 'Очень понравилось как звучит дудук. сделал его солирующим',
+            ru: 'Очень понравилось как звучит дудук. Сделал его солирующим',
         },
         audio: '/guitar_duduk_and_orchestra.mp3',
         cover: '/cover.jpg',
@@ -46,8 +46,8 @@ const tracks:ITrack[] = [
     {
         id: 4,
         title: {
-            en: 'Guitar and Orchestra, No. 3',
-            ru: 'Гитара и Оркестр, No 3',
+            en: 'Guitar and Orchestra,\nNo. 3',
+            ru: 'Гитара и Оркестр,\nNo 3',
         },
         description: {
             en: 'A simple piece to relieve attention',
@@ -60,8 +60,8 @@ const tracks:ITrack[] = [
     {
         id: 5,
         title: {
-            en: 'Guitar and Orchestra, No. 4',
-            ru: 'Гитара и Оркестр, No 4',
+            en: 'Guitar and Orchestra,\nNo. 4',
+            ru: 'Гитара и Оркестр,\nNo 4',
         },
         description: {
             en: 'Free form of the piece with increasing and decreasing tension',
