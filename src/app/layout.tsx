9f068eea864f48c85b0ddff57c68cc91509c5c85
Filipp Goriainov <barrios1991phil@gmail.com>
2024-07-15 23:10:50 +0400

    add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import { Nunito } from 'next/font/google';
 import './globals.css';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import { Metadata } from 'next';
 import { Player } from '@/widgets/Player';
 import clsx from 'clsx';
 import { ArticleThemeProvider } from '@/features/ThemeSwitcher';
 import { InnerLayout } from '@/app/_InnerLayout';
 import { LanguageProvider } from '@/features/LanguageSwitcher';
-import Script from 'next/script';
+import { Metrika } from './Metrika';
 
 const inter = Nunito({ weight: ['400', '700'], subsets: ['latin'] });
 
@@ -25,7 +25,9 @@ export default function RootLayout({
         <html lang="en">
 
             <body className={`${inter.className}`}>
-                <Script src="./script.js" />
+                <Suspense>
+                    <Metrika />
+                </Suspense>
                 <div className={clsx('player_wrapper', 'player_purple')}>
                     <LanguageProvider>
                         <Player />
