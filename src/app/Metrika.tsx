f90705e53727a764045142a30a50b6fa95161f6e
Filipp Goriainov <barrios1991phil@gmail.com>
2024-07-15 23:18:39 +0400

    add yandex metrika

--- src/app/Metrika.tsx
+++ src/app/Metrika.tsx
@@ -2,34 +2,17 @@
 
 import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
-import Script from 'next/script';
 
 declare let ym: any;
 
-export function Metrika() {
-    const pathName = usePathname();
+export default function Metrika() {
+    const pathname = usePathname();
     const searchParams = useSearchParams();
-    useEffect(() => {
-        ym('97839372', 'hit', window.location.href);
-    }, [pathName, searchParams]);
-    return (
-        <Script id="yandex-metrika">
-            {`
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(97839372, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</script>
-      `}
-        </Script>
-    );
+    useEffect(() => {
+        const url = `${pathname}?${searchParams}`;
+        ym(97839372, 'hit', url);
+    }, [pathname, searchParams]);
+
+    return null;
 }
