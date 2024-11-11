6ec12f957e13fa1dcaabf42652e08892666d15f8
Filipp Goriainov <barrios1991phil@gmail.com>
2024-11-11 20:13:46 +0400

    test ym

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 import { Nunito } from 'next/font/google';
 import './globals.css';
-import { ReactNode, Suspense } from 'react';
+import { ReactNode } from 'react';
 import { Metadata } from 'next';
 import { Player } from '@/widgets/Player';
 import clsx from 'clsx';
@@ -9,7 +9,6 @@ import { ArticleThemeProvider } from '@/features/ThemeSwitcher';
 import { InnerLayout } from '@/app/_InnerLayout';
 import { LanguageProvider } from '@/features/LanguageSwitcher';
 import Script from 'next/script';
-import Metrika from './Metrika';
 
 const inter = Nunito({ weight: ['400', '700'], subsets: ['latin'] });
 
@@ -34,7 +33,7 @@ export default function RootLayout({
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
  
-              ym(97839372, "init", {
+              ym(${process.env.YM_CODE}, "init", {
                     defer: true,
                     clickmap:true,
                     trackLinks:true,
@@ -42,9 +41,6 @@ export default function RootLayout({
                     webvisor:true
               });`}
                 </Script>
-                <Suspense fallback={<></>}>
-                    <Metrika />
-                </Suspense>
                 <div className={clsx('player_wrapper', 'player_purple')}>
                     <LanguageProvider>
                         <Player />
